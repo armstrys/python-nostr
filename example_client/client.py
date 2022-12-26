09c799050598ebb5f7b90a8163f2df88702dab45
Ryan Armstrong <ryscar13@gmail.com>
2022-12-26 15:27:26 -0800

    full text client running with posting

--- example_client/client.py
+++ example_client/client.py
@@ -11,10 +11,14 @@ assert 'python-nostr' in [Path(path).name for path in sys.path]
 ################################################################
 
 import warnings
+import json
+import time
+import os
 
 from nostr.key import PrivateKey, PublicKey
 from nostr.relay_manager import RelayManager
 from nostr.message_type import ClientMessageType
+from nostr.event import Event
 
 
 class Client:
@@ -52,7 +56,7 @@ class Client:
             relay_urls = [
                 'wss://nostr-2.zebedee.cloud',
                 'wss://nostr.zebedee.cloud',
-                'wss://nostr-relay.lnmarkets.com'
+                'wss://relay.damus.io'
             ]
         else:
             pass
@@ -60,6 +64,13 @@ class Client:
         self.relay_manager = RelayManager()
         for url in relay_urls:
             self.relay_manager.add_relay(url=url)
+    
+    @staticmethod
+    def event_to_message(event_type: ClientMessageType, event: Event):
+        return json.dumps([event_type, event])
+
+    def publish_to_relay(self, message: str):
+        self.relay_manager.publish_message(message)
 
 ######################### publish methods #######################
 # establishing methods for all possible events outlined here:   #
@@ -69,9 +80,13 @@ class Client:
     def publish_metadata(self):
         raise NotImplementedError()
     
-    def publish_text_note(self):
+    def publish_text_note(self, text):
         # TODO: need regex parsing to handle @
-        raise NotImplementedError()
+        event = Event(self.public_key.hex(), text)
+        event.sign(self.private_key.hex())
+        message = self.event_to_message(event_type=ClientMessageType.EVENT,
+                                        event=event.to_json_object())
+        self.publish_to_relay(message)
 
     def publish_recommended_relay(self):
         # TODO: need regex parsing to handle @
@@ -101,8 +116,6 @@ class Client:
     def publish_channel_metadata(self):
         raise NotImplementedError()
 
-    
-
     def send_encrypted_message(self):
         warnings.warn('''the current implementation of messages should be used with caution
                       see https://github.com/nostr-protocol/nips/issues/107''')
@@ -115,6 +128,55 @@ class ConnectedClient(Client):
         self.ssl_options = ssl_options
     def __enter__(self):
         self.relay_manager.open_connections(self.ssl_options)
+        time.sleep(2)
         return self
     def __exit__(self, type, value, traceback):
+        time.sleep(2)
         self.relay_manager.close_connections()
+
+
+class TextInputClient(ConnectedClient):
+    '''
+    a simple client that can be run as
+    ```
+    with TextInputClient():
+        pass
+    '''
+
+    def __enter__(self):
+        '''
+        using this object in a with statement
+        will open connections and run
+        '''
+        self.relay_manager.open_connections(self.ssl_options)
+        time.sleep(2)
+        self.run()
+        return self
+
+    def run(self):
+        cmd = 'start'
+        while cmd not in ['exit', 'x', '0']:
+            if cmd != 'start':
+                self.execute(cmd)
+            print(
+                '''select a command:
+                \t0\tE(x)it
+                \t1\tpublish note
+                '''
+            )
+            cmd = input().lower()
+        print('exiting')
+    
+    def execute(self, cmd):
+        if cmd == '1':
+            print(f'Enter a text note:\n')
+            text_note = input()
+            print(f'is this note correct?\n\n{text_note}')
+            response = input().lower()
+            if response in ['y', 'yes']:
+                self.publish_text_note(text_note)
+                print('published.')
+            else:
+                print('returning to menu')
+        else:
+            print('command not found. returning to menu')
